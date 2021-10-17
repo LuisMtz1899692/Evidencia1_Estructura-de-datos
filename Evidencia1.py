56ba014565a077eca423c33d04f2547a0d031ecc
Luis Alonso Martinez De La Cruz <luisalonso20101@hotmail.com>
2021-10-16 23:42:40 -0500

    Se Actualizo la mayoria del codigo

--- Evidencia1.py
+++ Evidencia1.py
@@ -1,59 +1,51 @@
 from collections import namedtuple
 from datetime import datetime
-datos_registro = {}
-salir=True
-nuevo_produc= True
-ventas= namedtuple("ventas", ("dia_venta","des_prod","Cant_produ", "Precio_product", "total_Iva"))
 
-while salir:
+diccionario = dict()
+lista_de_listas = list()
+
+while True:
     print("******Menu********")
     print("[1] Registrar una venta")
     print("[2] Consultar una venta")
     print("[3] Salir")
     print("******************")
-    resp_menu = input("¿Que desea realizar: ")
+    opcion = input("Elige una opcion :")
+    print()
+
+    if opcion == "1":
+        folio = input("Folio de la nueva venta: ")
+        while True:
+            ventas = namedtuple("ventas", ("fecha","producto","cantidad", "precio", "iva", "total"))
+            fecha = datetime.now()
+            producto = input("Producto: ")
+            precio = float(input("Precio: "))
+            cantidad = int(input("Cantidad: "))
+            iva = precio + (precio * 0.16)
+            total = iva * cantidad
+            eleccion = input("Desea Agregar un nuevo producto[S/N]")
+            print()
+            venta_registrada = ventas(fecha, producto, cantidad, precio, iva, total)
+            lista_de_listas.append(venta_registrada)
+            if eleccion == "S":
+                print("Agregue El Siguiente Producto")
+                pass
+            else:
+                break
+        diccionario[folio] = lista_de_listas
+        lista_de_listas = list()
+
+    if opcion == "2":
+        consulta = input("Ingrese el Folio a consultar: ")
+        campos = ("fecha","producto","cantidad", "precio", "iva", "total")
+        nombres = list()
+        for x in diccionario.keys():
+            if consulta == x:
+                """print(f"La Fecha registrada para esa venta es: {diccionario[consulta]}")"""
+                for tupla in diccionario.values():
+                    for dato in tupla:
+                        print(dato)
+    if opcion == "3":
+        print("Usted esta saliendo del programa")
+        break
     
-    if resp_menu == "1":
-        print("Bienvenido al menu Registrar venta")
-        dia_venta= datetime.now()
-        print(dia_venta)
-        Folio_Vet= input("Ingrese el Folio de la venta: ")
-        if not Folio_Vet in datos_registro.keys():
-            while nuevo_produc:
-                des_prod=input("Ingrese La Descripcion del Producto: ")
-                Cant_produ=int(input("Ingrese La Cantidad Del produtos: "))
-                Precio_product=int(input("Ingrese el Precio del producto: "))
-                res_producto = input("Desea Agregar un nuevo producto[S/N]")
-                if res_producto == "S":
-                    print("Agregue el siguiente producto: ")
-                elif res_producto=="N":    
-                    print("saliendo del menu registro")
-                    total=(Cant_produ)*(Precio_product)
-                    iva=(total)*(.16)
-                    total_Iva=(total)+(iva)
-                    venta_registrada = ventas(dia_venta, des_prod, Cant_produ, Precio_product, total_Iva)
-                    datos_registro[Folio_Vet] = venta_registrada
-                    print(f"El Total es: ${total}")
-                    print(f"El iva(16%): ${iva}")
-                    print(f"El total con iva es: ${total_Iva}")
-                    break
-        else:
-            print("la clave registrada ya existe, intente de nuevo")
-    elif resp_menu == "2":
-        print("Bienvenido al menu Consultar venta")
-        consulta=input("Ingrese el folio a buscar: ")
-        if consulta in datos_registro.keys():
-            print(f"Los Fecha registrada para esa venta es: {datos_registro[Folio_Vet].dia_venta}")
-            print(f"Los productos Registrados son: {datos_registro[Folio_Vet].des_prod}")
-            print(f"La Cantidad de productos es: {datos_registro[Folio_Vet].Cant_produ}")
-            print(f"El precio del productos es: {datos_registro[Folio_Vet].Precio_product}")
-            print(f"El total con iva Registrado es de: ${datos_registro[Folio_Vet].total_Iva}")
-        else:
-            print("El Folio ingrado no se encuentra en el sistema")
-            print("Intente de nuevo")
-    elif resp_menu == "3":
-        print("Usted esta saliendo del programa...")
-        salir=False
-    else:
-        print("La opcion ingresada no esta disponible")
-        print("Intente de nuevo")
